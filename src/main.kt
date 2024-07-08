02df6c4b244ea48e996800b3ebf91d717695adbf
Danggiang76x <144084605+Danggiang76x@users.noreply.github.com>
2024-07-08 21:45:55 +0700

    Update main.kt

--- src/main.kt
+++ src/main.kt
@@ -7,4 +7,5 @@ fun main() {
     println("Before accessing pi")
     println(pi)  // Output: Calculating PI 3.14159
     println(pi)  // Output: 3.14159 (không tính toán lại)
+    println(pi)  // Output: 3.14159 (không tính toán lại)
 }
